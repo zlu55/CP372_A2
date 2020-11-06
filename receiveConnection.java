7373ebbc63ecc18786ff68bc3c760e6556690efc
zlu <lulo9390@mylaurier.ca>
2020-11-06 17:30:13 -0500

    update

--- receiveConnection.java
+++ receiveConnection.java
@@ -9,7 +9,7 @@ public class receiveConnection{
 	
 	public void start(String IP, int sPort, int rPort, String outFile, boolean reliability, JTextArea displayACK) throws IOException{
 		System.out.println("Receiving...");
-		
+		//Set up socket and packet
 		socket = new DatagramSocket(null);
 		socket.bind(new InetSocketAddress(IP, rPort));
 		buf = new byte[1024];
@@ -28,19 +28,22 @@ public class receiveConnection{
 				
 				socket.receive(packet);
 				packetCounter++;
-				
+				//If it is using reliable data transfer, or if its not the 10th packet to be dropped...
 				if(reliability || packetCounter % 10 != 0){
 					StringBuilder tempData = new StringBuilder();
+					//Iterate through the packet data and add it to the tempData string
 					for(int i=0; i<packet.getLength(); i++){
 						if(packet.getData()[i] >= 9){
 							tempData.append((char) packet.getData()[i]);
 						}
 					}
+					//Add the tempData to the end data string
 					endData.append(tempData.toString());
 					
+					//get sequence number from the last char of the packet data
 					int seqNum = packet.getData()[packet.getLength() - 1];
 					
-					
+					//Check if the packet was an EOT datagram
 					if(tempData.toString().contains("\r") && seqNum == 5){
 						PrintWriter w = new PrintWriter(new FileWriter(outFile));
 						w.print(endData);
